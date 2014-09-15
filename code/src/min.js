e1cc64eb1f5c21872eb6b07c33a12aa364416f78
Matt Derasoirs <matt@planwork.us>
2014-09-15 17:42:17 -0400

    :seedling:
    problem 1 from Erdos

---
+++ code/src/min.js
@@ -0,0 +1,36 @@
+function MIN (n) {
+  
+  if (n < 1)
+    return undefined;
+
+  
+  var min_val = n*(n+1);
+
+  for (var i=0; i < 2000; i++) {
+    var total = 0;
+    var vals = random_array(n);
+    for (var k=0; k < n; k++) {
+      total += vals[k] * (k+1);
+    }
+
+    if (total >= 0 && total < min_val) {
+      min_val = total;
+    }
+  }
+
+  /**
+   * [random_array description]
+   * @param  {[type]} num [description]
+   * @return {[type]}     [description]
+   */
+  function random_array (num) {
+    var vals = new Array(num);
+    for (var i=0; i < vals.length; i++)
+      vals[i] = Math.random() > 0.5 ? +1 : -1;
+    return vals;
+  }
+
+  return min_val;
+}
+
+console.log(MIN(10000));
